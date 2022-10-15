4ccecec3fecee8ed13eccfdddd798f016ec23b45
Jezebel1990 <jezebelguedes@gmail.com>
2022-10-15 12:40:16 -0300

    config express server

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require("express");
+const { resolve, dirname } = require('path')
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 // server used to send send emails
 const app = express();
+
+app.use('/',
+ express.static(
+  resolve(
+    --dirname,
+    './build'
+  )
+ ))
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+// app.listen(5000, () => console.log("Server Running"));
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) { return console.log(err) }
+  console.log('Tudo funcionando certinho')
+})
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
-console.log('start')
+
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
