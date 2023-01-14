f2d14e5337849e1d3969663382a0c89e22faa0b5
Jezebel1990 <jezebelguedes@gmail.com>
2023-01-14 13:33:42 -0300

    add stack

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,20 @@
 import { Col } from "react-bootstrap";
+import icon1 from "../assets/img/icon1.svg";
+import icon2 from "../assets/img/icon2.svg";
 
-export const ProjectCard = ({ title, description, imgUrl, link }) => {
+export const ProjectCard = ({ title, description, imgUrl, link1,link2 }) => {
   return (
-    <Col size={12} sm={6} md={4} style={{ width: '32rem' }}>
+    <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
           <div className="proj-linkbx">
-            <a href={link} target="_blank">
-            <button className="button">
-                        Saiba mais
-                    </button>
-           </a>
+          </div>
+           <div className="social-icon">
+              <a href={link1}> <img src={icon1} alt="Icon" /></a>
+              <a href={link2}><img src={icon2} alt="Icon" /></a>
           </div>
         </div>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,12 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { Container, Row, Col, Tab } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
-import projImg4 from "../assets/img/project-img4.png";
-import projImg5 from "../assets/img/project-img5.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
+
 
 
 export const Projects = () => {
@@ -22,19 +18,23 @@ export const Projects = () => {
       title: "E-commerce Website",
       description: "Design & Desenvolvimento",
       imgUrl: projImg1,
-      link:"https://hw-tshirts.netlify.app",
+      link1:"https://github.com/LuaBezana/eCommerceGeneration",
+      link2:"https://hw-tshirts.netlify.app"
     },
     {
       title: "Rede Social",
       description: "Design & Desenvolvimento",
       imgUrl: projImg2,
-      link:"https://alurakut-blond-psi.vercel.app/login",
+      link1:"https://github.com/Jezebel1990/alurakut",
+      link2:"https://alurakut-blond-psi.vercel.app/login"
     },
-    // {
-    //   title: "Aplicativo",
-    //   description: "Design & Desenvolvimento",
-    //   imgUrl: projImg3,
-    // },
+    {
+      title: "Mobile",
+      description: "Design & Desenvolvimento",
+      imgUrl: projImg3,
+      link1:"https://github.com/Jezebel1990/appBank.git",
+      link2: ""
+    },
     // {
     //   title: "Mobile App",
     //   description: "Design & Development",
@@ -67,19 +67,9 @@ export const Projects = () => {
                 <h2>Projetos</h2>
                 <p>Tento constantemente garantir que as decisões de design sejam resultados de um processo centrado no ser humano. Esta é, uma coleção arquivada de projetos selecionados enquanto estava sob processo de aprendizagem.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Website</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Mobile</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Ux Design</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
+                
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
+                    
                       <Row>
                         {
                           projects.map((project, index) => {
@@ -92,38 +82,7 @@ export const Projects = () => {
                           })
                         }
                       </Row>
-                   </Tab.Pane>
-      <Tab.Pane eventKey="second">
-      <Card className="text-center" style={{ width: '32rem' }}>
-      <Card.Img variant="top" src={projImg3} />
-      <Card.ImgOverlay>
-      <Button variant="info"href="https://github.com/Jezebel1990/appBank.git">Saiba mais</Button>
-        {/* <Card.Title>Mobile</Card.Title> */}
-      </Card.ImgOverlay>
-    </Card>
-  </Tab.Pane>
-
-<Tab.Pane eventKey="third">
-<CardGroup>
-<Card className="text-center" style={{ width: '32rem' }}>
-      <Card.Img variant="top" src={projImg4} />
-      <Card.ImgOverlay>
-      <Button variant="info"href="https://docs.google.com/presentation/d/e/2PACX-1vQtaMkZW6Hg8dNqbms4Q69ndvXsf4W6dyLlJ4sSYMGtVMfVEMv16DCmNliSAtrkMguX91kDbKCKYa82/pub?start=false&loop=false&delayms=1000">Saiba mais</Button>
-        {/* <Card.Title>Mobile</Card.Title> */}
-      </Card.ImgOverlay>
-    </Card>
- 
-    <Card className="text-center" style={{ width: '32rem' }}>
-      <Card.Img variant="top" src={projImg5} />
-      <Card.ImgOverlay>
-      <Button variant="info"href="https://docs.google.com/presentation/d/e/2PACX-1vSBDBLb6gKka_8fEYKGClIry8YUsnZePuIq6WtrsF4RtD-6lpHkGleFCb5qqF3pAA/pub?start=true&loop=false&delayms=3000">Saiba mais</Button>
-        {/* <Card.Title>Mobile</Card.Title> */}
-      </Card.ImgOverlay>
-    </Card>
-    </CardGroup>
-
-
-</Tab.Pane>
+               
 </Tab.Content>
 </Tab.Container>
               </div>}
